dc81133f2ce6f2634d811a7444a31d715b6e5cf6
moha23 <mlahrech@outlook.com>
2024-12-29 13:53:44 -0500

    Task 3: Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,2 +1,12 @@
+import requests
+import json
+
 def emotion_detector(text_to_analyze):
-    return "success"
+    ''' Emotion detection application using the Watson NLP library 
+    '''
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = myobj, headers = header)
+    return response.text
