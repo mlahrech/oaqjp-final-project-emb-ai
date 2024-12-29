2aefc246120f985e7b5f563206b52d85a9dc85b3
moha23 <mlahrech@outlook.com>
2024-12-29 15:00:44 -0500

    Task 3: draft 1

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     ''' Emotion detection application using the Watson NLP library 
@@ -8,4 +9,22 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json = myobj, headers = header)
+    formatted_response = json.loads(response.text)
+    #emotions = ['emotionPredictions'][0]['emotion'].items()
+    #for emotion, score in emotions:
+    #    for emotion, score in emotions:
+    #for x in len(['emotionPredictions'][0]['emotion']):
+    #    emotions.append(['emotionPredictions'][0]['emotion'][x])
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    #for x in (anger_score, disgust_score, fear_score, joy_score, sadness_score):
+    #    if 
+
+    #dominant_emotion = max(anger_score, disgust_score, fear_score, joy_score, sadness_score)
+
     return response.text
