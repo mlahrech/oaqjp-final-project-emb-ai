91f88a6ab37be5b005fcaa470da57095b4f5d8d1
moha23 <mlahrech@outlook.com>
2024-12-30 03:34:39 -0500

    Task 6: Web deployment of the application using Flask

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emotion_detect():
+    ''' This code has a text input from the HTML interface and 
+        runs emotion detector. 
+        The output returned the result of the emotion analysis 
+        and mention the dominant oemotion
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    # Extracting the dominant emotion
+    dominant_emotion = response.pop("dominant_emotion")
+    # Constructing the response string
+    response_formatted = f"For the given statement, the system response is "
+    response_formatted += ", ".join([f"'{key}': {value}" for key, value in response.items()])
+    response_formatted += f". The dominant emotion is {dominant_emotion}."
+
+    return response_formatted
+
+@app.route("/")
+def render_index_page():
+    ''' This function renders the index.html page
+    '''
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
